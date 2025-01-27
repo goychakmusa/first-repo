db5774a8cab2ce0a3b9d08bf94f7e3d5b03238c8
Goychak Musayeva <goychak.musayeva@gmail.com>
2025-01-27 18:03:14 +0400

    version1

---
+++ src/main/java/first_git_package/MyGitClass.java
@@ -0,0 +1,12 @@
+package first_git_package;
+
+import java.sql.SQLOutput;
+
+public class MyGitClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Version 1");
+
+    }
+}
